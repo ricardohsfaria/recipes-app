1f45d4490f467b75fe329e4c019a6380ad39f90b
Mariana Pereira <marianapereira.s@live.com>
2023-01-26 19:12:45 -0300

    feat: Links e email na página Profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const user = localStorage.getItem('user');
+  const userObj = JSON.parse(user); // https://www.w3schools.com/js/js_json_stringify.asp
+
   return (
     <div>
       <fieldset>
         <legend>Profile</legend>
+        <p
+          data-testid="profile-email"
+        >
+          { userObj.email }
+        </p>
+        <a
+          href="/done-recipes"
+          data-testid="profile-done-btn"
+        >
+          <button>
+            Done Recipes
+          </button>
+        </a>
+        <a
+          href="/favorite-recipes"
+          data-testid="profile-favorite-btn"
+        >
+          <button>
+            Favorite Recipes
+          </button>
+        </a>
+        <a
+          href="/"
+          data-testid="profile-logout-btn"
+        >
+          <button
+            onClick={ () => localStorage.clear() }
+          >
+            Logout
+          </button>
+        </a>
       </fieldset>
       <Footer />
     </div>
