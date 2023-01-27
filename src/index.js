96a6149926e88620aa9667b7b32e9e4b356ed79c
Erick Lauretti <erick.nighyt@hotmail.com>
2023-01-27 15:46:30 -0300

    main-group-11-req10-recipes-fix-lint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { ContextProvider } from './context/ContextProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
